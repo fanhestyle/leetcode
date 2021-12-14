4f19beb917bfac9d5ef4e692191f82532e0e0e91
fanhe <southgis_Alex@126.com>
2021-12-14 08:25:41 +0800

    sudoku.

---
+++ sudoku_0037/soduku_1.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    static const int N = 9;
+
+    static void solveSudoku(vector<vector<char>>& board,std::ostream& os = std::cout) {
+
+    }
+
+    static bool isValid(int row, int column, char ch, vector<vector<char>>& board)
+    {
+
+    }
+
+};
