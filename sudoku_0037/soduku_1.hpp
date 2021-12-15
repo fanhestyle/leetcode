fd55363fb94cdcac80f59cbc3a97de66944eb8fc
fanhe <southgis_Alex@126.com>
2021-12-15 15:26:35 +0800

    sudoku.

--- sudoku_0037/soduku_1.hpp
+++ sudoku_0037/soduku_1.hpp
@@ -7,43 +7,47 @@ class Solution
 public:
     static const int N = 9;
 
-    static void solveSudoku(vector<vector<char>>& board,
-                            std::ostream& os = std::cout)
-    {
-        dfs(board, 0, 0, os);
-    }
+    static void solveSudoku(vector<vector<char>>& board) { dfs(board, 0, 0); }
 
-    static void dfs(std::vector<std::vector<char>>& board, int r, int c,
-                    std::ostream& os)
+    static bool dfs(std::vector<std::vector<char>>& board, int r, int c)
     {
         if (r == N)
-            return;
+            return true;
 
-        if (c < 9)
-            dfs(board, r, c + 1, os);
-        else
-            dfs(board, r + 1, c, os);
+        if (c == N)
+            return dfs(board, r + 1, 0);
 
-        for (int i = '1'; i <= '9'; i++)
+        if (board[r][c] != '.')
         {
-            if (isValid(r, c, i, board))
+            return dfs(board, r, c + 1);
+        }
+
+        for (int k = '1'; k <= '9'; k++)
+        {
+            if (isValid(r, c, k, board))
             {
-                
+                board[r][c] = k;
+                if (dfs(board, r, c + 1))
+                    return true;
+                board[r][c] = '.';
             }
         }
+        return false;
+
+        return false;
     }
 
     static bool isValid(int row, int col, char ch, vector<vector<char>>& board)
     {
         for (int i = 0; i < N; i++)
         {
-            if (i != col && ch == board[row][i])
+            if (ch == board[row][i])
                 return false;
         }
 
         for (int j = 0; j < N; j++)
         {
-            if (j != row && ch == board[j][col])
+            if (ch == board[j][col])
                 return false;
         }
 
@@ -51,7 +55,7 @@ public:
         {
             for (int j = (col / 3) * 3; j <= (col / 3) * 3 + 2; j++)
             {
-                if (i != row && j != col && board[i][j] == ch)
+                if (board[i][j] == ch)
                     return false;
             }
         }
